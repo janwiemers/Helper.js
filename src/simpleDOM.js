e5efee37911147773b5b0e9734496ea44bcb2014
Jan Wiemers <j.wiemers@allerdesign.de>
2013-09-06 10:51:04 +0200

    New Version
    add some functions like, hasClass, removeClass and some others

--- src/simpleDOM.js
+++ src/simpleDOM.js
@@ -1,156 +1,189 @@
-/**
- * simpleDOM
- * A very small DOM Manipulating Library
- *
- * @Version: 0.1 alpha
- * @Maintainer: Jan Wiemers
- * @Package: simpleDOM
- */
-
 (function(){
-
-    $ = simpleDOM = function(selector, context) {
-        if (!(this instanceof simpleDOM)) {
-            return new simpleDOM(selector, context);
-        }
-
-        this.document = document;
-        this.window   = window
-        this.elems    = [];
-        this.context  = (context !== undefined) ? context : document;
-        this.DOMReady = false;
-
-        /* Is the Sector a Function add it to the DOMReadyList */
-        if(typeof selector === 'function') { this.ready(selector); }
-        
-        /* else Execute the Query on the Context */
-        var _elemsHolder = this.context.querySelectorAll(selector);
-
-        try {
-            this.elems = Array.prototype.concat.call(_elemsHolder);
-        } catch (ex) {
-
-            for (var i in _elemsHolder) {
-                _elemsHolder.hasOwnProperty(i) && this.elems.push(_elemsHolder[i]);
+    $ = Helper = function(selector, context) {
+        if (!(this instanceof Helper))  return new Helper(selector, context);
+        this.readyList     = [];
+        this.isReady       = false;
+        this.eventbindings = [];
+        this.elem          = false;
+        if ( typeof ( selector ) == 'function' ) { this.ready(selector); }
+        else if ( typeof selector == 'object' )  { this.elem = selector; }
+        else {
+            var c = context || document,
+                e = [],
+                i,
+                elms,
+                initial;
+            if      ( selector === window || selector === document )  { this.elem = selector; }
+            else if ( selector === 'body' )                           { this.elem = c.body; }
+            else {
+                initial = selector.substr(0,1);
+                if ( initial === '#' )    { this.elem = c.getElementById(selector.slice(1)); }
+                else if ( initial === '.' ) {
+                    elms = (!!c.getElementsByClassName) ? c.getElementsByClassName(selector.slice(1)) : c.querySelectorAll(selector);
+                    for(i=0; i<elms.length; i++) {
+                        e.push(elms[i]);
+                    }
+                    this.elem = e;
+                }
+                else {
+                    elms = c.getElementsByTagName(selector);
+                    for(i=0; i<elms.length; i++) {
+                        e.push(elms[i]);
+                    }
+                    this.elem = e;
+                }
             }
+            return (this.elem === null) ? false : this;
         }
-
-
     };
-
-    simpleDOM.prototype = {
-
-        /**
-         * Add the Document Ready event
-         */
-        checkDOMReady: function () {
-
-            if (!this.DOMReady) {
-                if (this.document.addEventListener) {
-                    DOMContentLoaded = function (scope) {
-                        return function () {
-                            this.document.removeEventListener("DOMContentLoaded", DOMContentLoaded, false);
-                            scope.setDOMReady();
-                        };
-                    }(this);
-                } else {
-                    if (this.document.attachEvent) {
-                        DOMContentLoaded = function (scope) {
-                            return function () {
-                                if (this.document.readyState === "complete") {
-                                    this.document.detachEvent("onreadystatechange", DOMContentLoaded);
-                                    scope.setDOMReady();
-                                }
-                            };
-                        }(this);
-                    }
+    Helper.prototype = {
+        checkDocumentReady: function () {
+            if(!this.isReady) {
+                if ( document.addEventListener ) {
+                DOMContentLoaded = function(c) {
+                    return function(){
+                        document.removeEventListener( "DOMContentLoaded", DOMContentLoaded, false );
+                        c.setReady();
+                }}(this);
                 }
-                if (this.document.readyState === "complete") {
-                    this.setDOMReady();
+                else if ( document.attachEvent ) {
+                    DOMContentLoaded = function(c) {
+                        return function() {
+                            if ( document.readyState === "complete" ) {
+                                document.detachEvent( "onreadystatechange", DOMContentLoaded );
+                                c.setReady();
+                            }
+                    }}(this);
                 }
-                if (this.document.addEventListener) {
-                    this.document.addEventListener("DOMContentLoaded", DOMContentLoaded, false);
-                    this.window.addEventListener("load", this.setDOMReady, false);
-                } else {
-                    if (this.document.attachEvent) {
-                        this.document.attachEvent("onreadystatechange", DOMContentLoaded);
-                        this.window.attachEvent("onload", this.setDOMReady);
-                    }
+                if ( document.readyState === "complete" ) {
+                    this.setReady();
+                }
+                if ( document.addEventListener ) {
+                    document.addEventListener( "DOMContentLoaded", DOMContentLoaded, false );
+                    window.addEventListener( "load", this.setReady, false );
+                 } else if ( document.attachEvent ) {
+                        document.attachEvent("onreadystatechange", DOMContentLoaded);
+                        window.attachEvent( "onload", this.setReady );
                 }
             }
         },
-
-        /**
-         * Executes the Functions applyed
-         * @return {[type]} [description]
-         */
-        executeDOMReadyFunctions: function () {
-            for (var i = 0; i < this.DOMReadyList.length; i++) {
-                (new this.DOMReadyList[i]());
+        executeReadyFunction: function() {
+            for (var i = 0; i < this.readyList.length; i++) {
+                (new this.readyList[i]());
             }
         },
-
-        /**
-         * Sets the Document Ready Function to true
-         */
-        setDOMReady: function () {
-            this.DOMReady = true;
+        setReady: function() {
+            this.isReady = true;
         },
-
-        /**
-         * Public function to add Functions to the DocuementReady Event
-         * @param  function
-         */
-        ready: function (fn) {
-            if (typeof fn !== "function") {
-                return;
-            }
-            if (this.DOMReady) {
+        ready: function ( fn ) {
+            if(typeof fn !== 'function') return;
+            if(this.isReady) {
                 return fn();
             }
-            this.DOMReadyList.push(fn);
-            if (!this.DOMReadyCheckRunning) {
-                this.DOMReadyCheckRunning = true;
-                this.DOMReadyInterval = window.setInterval(function (scope) {
-                    return function () {
-                        scope.checkDOMReady();
-                        if (scope.DOMReady) {
-                            clearInterval(scope.DOMReadyInterval);
-                            scope.executeDOMReadyFunction();
-                        }
-                    };
-                }(this), 9);
+            this.readyList.push(fn);
+            if(!this.documentReadyCheckRunnig) {
+                this.documentReadyCheckRunnig = true;
+                this.readyInterval = window.setInterval(function(c) { return function(){
+                    c.checkDocumentReady();
+                    if(c.isReady) {
+                        clearInterval(c.readyInterval);
+                        c.executeReadyFunction();
+                    }
+                }}(this), 9);
             }
         },
-
-        /**
-         * Returns the indexed Item of the Current Collection
-         * @param  integer index  [The Item id starts at 0]
-         * @return Array Index
-         */
-        eq: function( index ) {
-            return this.elems[index];
-        },
-
-        /**
-         * Execute a for loop
-         * @param  {Function} callback [description]
-         * @return {[type]}            [description]
-         */
-        each: function( callback ){
-
-            var _length = this.elems.length,
-                _i      = 0;
-
-            while (_i < _length) {
-                _i++;
-                callback.call(this.elems[_i], _i);
+        on: function(event,cb) {
+            if (typeof cb != 'function' || this.elem === null) return;
+            var elem = this.elem;
+            if ( this.elem.addEventListener ) {
+                this.elem.addEventListener(event, function(event) {
+                    event               = event || window.event;
+                    event.target = event.target || event.currentTarget || event.srcElement || elem;
+                    cb.call( elem, event );
+                },false);
+            } else if ( this.elem.attachEvent ) {
+                this.elem.attachEvent('on' + event, function(event) {
+                    event               = event || window.event;
+                    event.target = event.target || event.currentTarget || event.srcElement || elem;
+                    cb.call( elem, event );
+                });
             }
-
+        },
+        stopPropagation: function (e) {
+           return (e.stopPropagation) ? e.stopPropagation() : e.cancelBubble = true;
+        },
+        css:function(property,value) {
+            if ( property && value ){
+                this.elem.style[property] = value;
+            }
+            else if ( property ) {
+                return this.elem.style[property];
+            }
+        },
+        fade: function ( d ) {
+            if (!d || !this.elem) return;
+            var c = (this.elem.length) ? this.elem : [this.elem];
+            window.setTimeout(function(c){ return function(){
+                for ( i in c ) {
+                    c[i].className += ' tr tra';
+                }
+            }}(c), 100);
+            if(d == 'out') {
+                window.setTimeout(function(c){ return function(){
+                    for ( i in c ) {
+                        c[i].className = c[i].className.replace('tra', '');
+                    }
+                }}(c), 100);
+            }
+            return this;
+        },
+        attr: function (a) {
+            if(this.elem && a) return this.elem.getAttribute(a) || false;
+        },
+        remove: function() {
+            return (this.elem.parentNode.removeChild(this.elem));
+        },
+        addElement: function( t, v, o ) {
+            var nn = document.createElement(t);
+            if(v) { 'innerText' in nn ? (nn.innerText = v) : (nn.innerHTML = v); }
+            if(o) { for(i in o) { nn[o[i][0]] = o[i][1]; }}
+            return (this.elem.parentNode.insertBefore(nn, this.elem));
+        },
+        hasClass: function (c) {
+            if(!this.elem.className) return false;
+            return !!this.elem.className.match(new RegExp('(^|\\s)'+c+'(\\s|$)', 'g'));
+        },
+        removeClass: function( c ) {
+            return this.elem.className = this.elem.className.replace(new RegExp('(^|\\s)'+c+'(\\s|$)', 'g'), ' ');
+        },
+        addClass: function( c ) {
+            return this.elem.className += ' '+c;
+        },
+        toggleClass: function( c ) {
+            return ( $(this.elem).hasClass( c ) ) ? $(this.elem).removeClass( c ) : $(this.elem).addClass( c );
         }
-
-
-
     };
-
+    $.ajax = function(u, c, s) {
+        var async = (s && s === false) ? false : true;
+        var xhr=window.ActiveXObject ? new ActiveXObject("Microsoft.XMLHTTP") : new XMLHttpRequest();
+        xhr.open('GET', u, async);
+        if (c && typeof c === 'function') xhr.onreadystatechange = function() {
+            if(xhr.readyState == 4) c(xhr);
+        };
+        xhr.send(null);
+        return xhr;
+    };
+    $.track = function(c, t, params) {
+        var url  = 'stats?section=search'+c+'.'+t,
+            keys = [],
+            i;
+        if(params && typeof params === "object" ) {
+            keys = Object.keys(params);
+            for (i=0; i<keys.length; i++) {
+                url += '&'+keys[i]+'='+params[keys[i]];
+            }
+        }
+        url += '&mts='+(new Date()*1);
+        this.ajax(url, '', false);
+    };
 })();
